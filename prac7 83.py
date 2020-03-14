93b4c11c4d822381afa887e8343e0d2fc075b1ed
taksh786 <61076016+taksh786@users.noreply.github.com>
2020-03-14 12:17:44 +0530

    Add files via upload

---
+++ prac7 83.py
@@ -0,0 +1,12 @@
+class student:
+    def __init__(self,rno):
+        
+        self.rno=rno
+        
+    def display(self):
+        print('roll no is',self.rno)
+
+r=student(83)
+r.display()
+
+    
